a87637729d3d2b33b7baff2ccb0b02cea3a31455
Will Miller <millerw4@miamioh.edu>
2024-01-27 16:15:03 -0500

    workaround for mouse resolution controls

--- Assets/MousePlayer.cs
+++ Assets/MousePlayer.cs
@@ -5,10 +5,29 @@ using UnityEngine.InputSystem;
 
 public class MousePlayer : MonoBehaviour
 {
+    public float speed = 1f;
+    bool isMoving = false;
+    Vector2 mousePosition;
+    Vector2 mousePositionOld;
+    public float maxXResolution;
+    public float maxYResolution;
+    float mouseNormalizedX;
+    float mouseNormalizedY;
+    public Transform sword;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var mouse = Mouse.current;
+        mousePosition = mouse.position.ReadValue();
+        mousePositionOld = mousePosition;
+        if (maxXResolution == 0)
+        {
+            maxXResolution = Screen.currentResolution.width;
+        }
+        if (maxYResolution == 0)
+        {
+            maxYResolution = Screen.currentResolution.height;
+        }
     }
 
     // Update is called once per frame
@@ -23,13 +42,75 @@ public class MousePlayer : MonoBehaviour
         {
             OnRightClick();
         }
+        mousePositionOld = mousePosition;
+        mousePosition = mouse.position.ReadValue();
+        if (mousePosition.x < 0)
+        {
+            mousePosition.x = 0;
+        }
+        if (mousePosition.x > maxXResolution)
+        {
+            mousePosition.x = maxXResolution;
+        }
+        if (mousePosition.y < 0)
+        {
+            mousePosition.y = 0;
+        }   
+        if (mousePosition.y > maxYResolution)
+        {
+            mousePosition.y = maxYResolution;
+        }
+        mouseNormalizedX = mousePosition.x / maxXResolution;
+        mouseNormalizedY = mousePosition.y / maxYResolution;
+        sword.localPosition = new Vector3(mouseNormalizedX - 0.5f, 0, mouseNormalizedY + 0.5f);
+    }
+    void LogMousePosition()
+    {
+        if (mousePositionOld != mousePosition)
+        {
+            Debug.Log(mousePosition);
+            Debug.Log(maxXResolution);
+            Debug.Log(maxYResolution);
+        }
     }
     void OnLeftClick()
     {
-        Debug.Log("Left Click");
+        Advance();
     }
     void OnRightClick()
     {
-        Debug.Log("Right Click");
+        Retreat();
+    }
+    void Advance()
+    {
+        if (isMoving)
+        {
+            return;
+        } else
+        {
+            isMoving = true;
+            StartCoroutine(Move(transform.position + Vector3.left * speed));
+        }
+    }
+    void Retreat()
+    {
+        if (isMoving)
+        {
+            return;
+        }
+        else
+        {
+            isMoving = true;
+            StartCoroutine(Move(transform.position + Vector3.right * speed));
+        }
+    }
+    IEnumerator Move(Vector3 destination)
+    {
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * 2 * Time.deltaTime);
+            yield return null;
+        }
+        isMoving = false;
     }
 }
